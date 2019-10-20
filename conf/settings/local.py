5cbf3f88eddf5ab966b67824fd346db38cdf25a2
pincoin <pincoins@gmail.com>
2019-10-20 15:50:18 +0700

    - static settings

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -1,3 +1,18 @@
 from .base import *
 
 DEBUG = True
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, Javascript, Images)
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'conf', 'static'),
+    os.path.join(BASE_DIR, 'golf', 'static'),
+]
