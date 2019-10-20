b0fbe07cd94e825642904d93e3e2da68cb6812e7
pincoin <pincoins@gmail.com>
2019-10-21 00:49:24 +0700

    - model utils / mptt

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -23,9 +23,9 @@ TIME_ZONE = 'Asia/Bangkok'
 STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 STATICFILES_DIRS = [
+    '/Users/mairoo/.pyenv/versions/toj/lib/python3.6/site-packages/django/contrib/admin/static',
     os.path.join(BASE_DIR, 'conf', 'static'),
     os.path.join(BASE_DIR, 'golf', 'static'),
-    '/Users/mairoo/.pyenv/versions/toj/lib/python3.6/site-packages/django/contrib/admin/static',
 ]
 
 # Media files (Uploaded files)
