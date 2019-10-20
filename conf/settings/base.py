2f1e84ad5b4849152b44ed3fd112445eeeffbfb9
pincoin <pincoins@gmail.com>
2019-10-20 22:46:32 +0700

    - admin static files

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -37,7 +37,8 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -50,20 +51,17 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 WSGI_APPLICATION = 'conf.wsgi.application'
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
--- conf/settings/local.py
+++ conf/settings/local.py
@@ -1,13 +1,23 @@
+from django.utils.translation import ugettext_lazy as _
+
 from .base import *
 
 DEBUG = True
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ko-KR'
+LANGUAGES = [
+    ('ko', _('Korean')),
+    ('th', _('Thai')),
+    ('en', _('English')),
+]
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+TIME_ZONE = 'Asia/Bangkok'
 
 # Static files (CSS, Javascript, Images)
 STATIC_URL = '/assets/'
@@ -15,4 +25,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'conf', 'static'),
     os.path.join(BASE_DIR, 'golf', 'static'),
+    '/Users/mairoo/.pyenv/versions/toj/lib/python3.6/site-packages/django/contrib/admin/static',
 ]
+
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
