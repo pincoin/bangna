32043c038ac23b0a5ed93975ab424f8dd6bbaa1f
pincoin <pincoins@gmail.com>
2019-10-29 22:06:35 +0700

    - email settings

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -9,6 +9,14 @@ SECRET_KEY = secret['SECRET_KEY']
 ALLOWED_HOSTS = secret['ALLOWED_HOSTS']
 DATABASES = secret['DATABASES']
 
+EMAIL_HOST = secret['EMAIL_HOST']
+EMAIL_HOST_USER = secret['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = secret['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = secret['EMAIL_PORT']
+EMAIL_USE_TLS = secret['EMAIL_USE_TLS']
+EMAIL_NO_REPLY = secret['EMAIL_NO_REPLY']
+EMAIL_CUSTOMER_SERVICE = secret['EMAIL_CUSTOMER_SERVICE']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -79,3 +87,35 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
+
+# django.contrib.auth settings for allauth
+PASSWORD_RESET_TIMEOUT_DAYS = 1  # default=3
+LOGIN_URL = '/accounts/login/'  # default=/accounts/login/
+LOGOUT_URL = '/accounts/logout/'  # default=/accounts/logout/
+LOGIN_REDIRECT_URL = '/'  # default=/accounts/profile/
+# LOGOUT_REDIRECT_URL = '/'
+
+# django-allauth
+DEFAULT_FROM_EMAIL = EMAIL_NO_REPLY
+# ACCOUNT_ADAPTER = 'member.adapters.MyAccountAdapter'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300
+# ACCOUNT_SIGNUP_FORM_CLASS = 'member.forms2.MemberSignupForm'
+ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE = True  # default=False
+SOCIALACCOUNT_AUTO_SIGNUP = False
+
+# Social providers for django-allauth
+# Each key has an empty dictionary value that will eventually contain provider specific configuration options by admin
+SOCIALACCOUNT_PROVIDERS = {
+    'facebook': {},
+    'line': {
+        'SCOPE': [
+            'profile',
+            'openid',
+            'email',
+        ],
+    },
+}
